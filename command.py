8137714d866a6db0daea7084687a119c6532672e
light <1870499383@qq.com>
2016-12-05 15:32:38 +0800

    bug

--- command.py
+++ command.py
@@ -7,7 +7,7 @@ roslaunch turtlebot_bringup minimal.launch
 roslaunch turtlebot_navigation amcl_demo.launch map_file:=/home/turtlebot/map/floor2.yaml
 
 roslaunch myturtlebotpack my.launch
-roslaunch myturtlebotpack
+roslaunch myturtlebotpack amcl_demo.launch
 
 
 """

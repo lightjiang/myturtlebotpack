bcb57a0d75432448b716ffaec34b34b605f0e0a4
light <1870499383@qq.com>
2016-12-05 15:29:06 +0800

    bug

--- command.py
+++ command.py
@@ -6,5 +6,8 @@ rosrun rqt_graph rqt_graph
 roslaunch turtlebot_bringup minimal.launch
 roslaunch turtlebot_navigation amcl_demo.launch map_file:=/home/turtlebot/map/floor2.yaml
 
+roslaunch myturtlebotpack my.launch
+roslaunch myturtlebotpack
+
 
 """
